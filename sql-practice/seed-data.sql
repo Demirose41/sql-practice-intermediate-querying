7e89fde8aa0db57e385eabc9e8ffcb28016d23fe
demirose41 <demirose41@gmail.com>
2024-12-06 00:37:56 -0800

    completed bonus practice

--- sql-practice/seed-data.sql
+++ sql-practice/seed-data.sql
@@ -28,3 +28,14 @@ SELECT * FROM albums WHERE num_sold >= 100000;
  SELECT * FROM albums WHERE year BETWEEN 2018 AND 2020;
   -- Query 3: SELECT for attributes matching a list of values with band_ids of either 1, 3, or 4.
 SELECT * FROM albums WHERE band_id IN (1, 3, 4);
+
+-- INTERMEDIATE Query 1: SELECT for attributes matching a pattern
+SELECT * FROM albums WHERE titles LIKE 'The%';
+
+-- INTERMEDIATE Query 2: SELECT ordered data
+-- Run the SQL command that returns the albums that have the two highest sales numbers.
+SELECT * FROM albums ORDER BY num_sold DESC LIMIT 2;
+
+-- ADVANCED Query 3: SELECT in the middle of ordered data
+-- Run the SQL command that returns the next two highest sales numbers (only the third and fourth highest sales).
+SELECT * FROM albums ORDER BY num_sold DESC LIMIT 2 OFFSET 2;
